b1b1a390ab601c7792647b2ef003411908747350
Vladimir Kaydalin <exfinancier@gmail.com>
2021-08-22 14:20:58 +0300

    router added

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,21 +4,16 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/gorilla/mux"
-	"github.com/pkg/errors"
-
-	"github.com/spinel/go-musthave-shortener-tpl/internal/app/handler"
 	"github.com/spinel/go-musthave-shortener-tpl/internal/app/repository"
+	"github.com/spinel/go-musthave-shortener-tpl/internal/app/router"
 )
 
 func main() {
 	repo, err := repository.New()
 	if err != nil {
-		errors.Wrap(err, "store")
+		panic("no repo")
 	}
-	r := mux.NewRouter()
-	r.HandleFunc("/", handler.CreateUserHandler(repo))
-	r.HandleFunc("/{id:[0-9a-z]+}", handler.GetUserHandler(repo))
-	http.Handle("/", r)
+
+	http.Handle("/", router.Router(repo))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ internal/app/router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/spinel/go-musthave-shortener-tpl/internal/app/handler"
+	"github.com/spinel/go-musthave-shortener-tpl/internal/app/repository"
+)
+
+func Router(repo *repository.Store) *mux.Router {
+	r := mux.NewRouter()
+	//r.HandleFunc("/map", handler.ListUserHandler(repo))
+	r.HandleFunc("/", handler.CreateUserHandler(repo))
+	r.HandleFunc("/{id:[0-9a-z]+}", handler.GetUserHandler(repo))
+	return r
+}
